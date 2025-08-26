a64534bd2f31a03bcb420836ede66c303fbf83b9
cy <chenyang178@163.com>
2025-08-26 09:05:32 +0800

    基于spring AI框架实现多轮对话

---
+++ src/test/java/com/cy/dcaiagent/app/LoveAppTest.java
@@ -0,0 +1,33 @@
+package com.cy.dcaiagent.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void doChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮对话
+        String message = "你好，我是大陈";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮对话
+        message = "我想让我的另一半小常更爱我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮对话
+        message = "我的另一半叫什么名字来着，刚才告诉过你";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
